50d19f4588aa5221bef86f0b14d3d4b1478d1dc4
vikander <vikander.erik@gmail.com>
2016-01-27 17:54:52 +0700

    Lesson 03

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 class App extends React.Component {
 	render() {
-		return <div>Hi there!</div>
+		// The element we want to create, props, inner HTML
+		// Cannot return several dom nodes, as it would be the equivalent of returning
+		// multiple values from a function. Wrapping everything in an enclosing div is 
+		// however possible.
+
+		// return React.createElement('h1', null, 'Hello Guys!')
+		return ( // Wrap in paranthesis to utilize whitespace
+			<div>
+				<h1>Hello World</h1>
+				<b>Bold</b>
+			</div>
+		);
 	}
 }
+
+// Stateless function component (Does not have state)
+// const App = () => <h1>Hello brothers and sisters!</h1>
 
 export default App
