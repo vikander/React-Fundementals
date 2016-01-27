e07fed34c70c063f2c75a3c2e5df7490c5eb2bc1
vikander <vikander.erik@gmail.com>
2016-01-27 18:57:40 +0700

    Lesson 4

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 class App extends React.Component {
 	render() {
 		// The element we want to create, props, inner HTML
@@ -7,16 +8,40 @@ class App extends React.Component {
 		// however possible.
 
 		// return React.createElement('h1', null, 'Hello Guys!')
+
+		let animal = this.props.animal
+		let numberOfLives = this.props.numberOfLives
+		let country = this.props.country
+
 		return ( // Wrap in paranthesis to utilize whitespace
 			<div>
 				<h1>Hello World</h1>
 				<b>Bold</b>
+				<br/>
+				<span>
+					An {country} {animal} has {numberOfLives} lives
+				</span>
 			</div>
 		);
 	}
 }
 
+// Define the prop types that we are expecting (Just an object of our component, where each
+// key corrsponds to the prop name, and the value is the type, from React)
+
+App.propTypes = {
+	animal: React.PropTypes.string,
+	numberOfLives: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	country: 'African'
+}
+
+
+ReactDOM.render(<App animal="cat" numberOfLives={9} />, document.getElementById('app'));
+
 // Stateless function component (Does not have state)
 // const App = () => <h1>Hello brothers and sisters!</h1>
 
-export default App
+// export default App
